a69ce1239f82a1f18a1c4828145898e0ef66be0e
sgreilick <samgreilick@gmail.com>
2020-10-06 00:42:08 -0600

    added manager function, thanks yakini

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const Manager = require("./lib/Manager");
-const Engineer = require("../Develop/lib/Employee");
+const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
@@ -7,29 +7,51 @@ const fs = require("fs");
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
+const render = require("./lib/newRenderer");
 
+//Thank you Dilan and Yakini, I couldn't have done it without your help
 const render = require("./lib/htmlRenderer");
+var count = 0;
+const employee = [];
+const empIdArray = [];
 
 
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
+ function Managerfn() {
+   inquirer.prompt([
+      {
+        type: "input",
+        name: "ManagerName",
+        message: "What is your Manager name?",
+        validate: answerValidation
+      },
+      {
+        type: "input",
+        message: "What is your Manager Id?",
+        name: "managerId",
+        validate: idValidation
+      },
+      {
+        type: "input",
+        message: "What is your Manager's email?",
+        name: "managerEmail",
+        validate: emailValidation
+      },
+      {
+        type: "input",
+        message: "What is your Manager's office number?",
+        name: "managerNo",
+        validate: phNoValidation
 
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
+      }
+    
+    ]).then(function(answers){
+   
+         const manager = new Manager(answers.managerName ,answers.managerId, answers.managerEmail, answers.managerNo )
+         employee.push(manager);
+         empIdArray.push(answers.managerId);
+         Teamfn();
 
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
+        }) .catch(function(err) {
+      console.log(err);
+    });
+  }
